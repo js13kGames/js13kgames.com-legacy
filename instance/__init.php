75fde36c6b049f6d5758c9c64f45c1cf6c873772
Michał Chojnacki <m.chojnacki@muyo.io>
2014-07-08 15:24:34 +0200

    A little extra precaution due to regex galore in Nginx' config.

--- instance/__init.php
+++ instance/__init.php
@@ -9,6 +9,13 @@
 		'storage' => $baseDir.'/instance/storage',
 	], isset($_SERVER['JS13K_ENV']) ? $_SERVER['JS13K_ENV'] : 'production');
 
+	// If by any chance the filters failed and we ended up with a wildcard request, override the subdomain based
+	// edition pick with the default edition.
+	if(!in_array($_SERVER['JS13K_EDITION'], ['2014', '2013', '2012']))
+	{
+		$_SERVER['JS13K_EDITION'] = $kernel->make('config')->get('games.edition_slug');
+	}
+
 	// Register our routes no sooner than just before a Request gets handled.
 	$kernel->on(nyx\framework\definitions\Events::REQUEST_RECEIVED, function($event) use($kernel) {
 
