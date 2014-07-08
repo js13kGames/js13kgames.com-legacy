b10eb2f054d60deccb7b4243e9f6a363fcdc7021
Michał Chojnacki <m.chojnacki@muyo.io>
2014-07-08 07:59:34 +0200

    Data models/migrations/seeds structure and Base model.

---
+++ src/data/models/Base.php
@@ -0,0 +1,16 @@
+<?php namespace js13kgames\data\models;
+
+	/**
+	 * Abstract Base Model
+	 *
+	 * @package     Js13kgames\Data\Models
+	 * @version     0.0.1
+	 * @author      Michal Chojnacki <m.chojnacki@muyo.pl>
+	 * @copyright   2012-2014 js13kGames Team
+	 * @link        http://js13kgames.com
+	 */
+
+	class Base extends \Eloquent
+	{
+
+	}
