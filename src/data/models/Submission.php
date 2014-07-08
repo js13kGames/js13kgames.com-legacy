cff82ec31807965186b349117077ca0fbc20bdba
Michał Chojnacki <m.chojnacki@muyo.io>
2014-07-08 10:04:49 +0200

    [Controllers] Keep validation within the Submission model.

--- src/data/models/Submission.php
+++ src/data/models/Submission.php
@@ -34,6 +34,45 @@
 		 *
 		 */
 
+		public static function getValidationMessages()
+		{
+			return [
+				'spam.required' => 'You didn\'t do the math. Please fill the spam protection field.',
+				'spam.spam'     => 'Your math is off. Please try again. Harder, this time.',
+				'unique_slug'   => 'A game with the same name has already been submitted. Please choose a different name.',
+				'reserved_slug' => 'The title of the game must not be one of: 2012, 2013 or 2014.',
+				'if_server'     => 'A valid URL to the deployed game and a .zip with the server code must be provided for a game in the "Server" category.',
+			];
+		}
+
+		/**
+		 *
+		 */
+
+		public static function getValidationRules()
+		{
+			return [
+				'spam'             => 'required|spam',
+				'token'            => 'required',
+				'author'           => 'required|min:3',
+				'email'            => 'required|email',
+				'categories'       => 'required',
+				'website_url'      => 'url',
+				'github_url'       => 'url',
+				'server_url'       => 'url|if_server',
+				'title'            => 'required|min:2|max:100|unique_slug|reserved_slug',
+				'description'      => 'required|min:10|max:1000',
+				'file'             => 'mimes:zip|max:13',
+				'file_server'      => 'mimes:zip|max:13|if_server',
+				'small_screenshot' => 'mimes:jpeg,gif,png|max:100',
+				'big_screenshot'   => 'mimes:jpeg,gif,png|max:100'
+			];
+		}
+
+		/**
+		 *
+		 */
+
 		public function uri()
 		{
 			return '/entries/'.$this->slug;
