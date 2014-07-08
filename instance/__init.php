6464dbdd91172064f21b1740d02c5b24e93ab936
Michał Chojnacki <m.chojnacki@muyo.io>
2014-07-08 13:54:44 +0200

    Start wiring up some routes.

--- instance/__init.php
+++ instance/__init.php
@@ -1,4 +1,6 @@
-<?php return new nyx\framework\Kernel([
+<?php
+
+	$kernel = new nyx\framework\Kernel([
 		'base'    => $baseDir,
 		'app'     => $baseDir.'/instance',
 		'config'  => $baseDir.'/instance/config',
@@ -6,3 +8,26 @@
 		'src'     => $baseDir.'/src',
 		'storage' => $baseDir.'/instance/storage',
 	], isset($_SERVER['JS13K_ENV']) ? $_SERVER['JS13K_ENV'] : 'production');
+
+	// Register our routes no sooner than just before a Request gets handled.
+	$kernel->on(nyx\framework\definitions\Events::REQUEST_RECEIVED, function($event) use($kernel) {
+
+		$router = $kernel->make('router');
+
+
+		$router->group(['namespace' => 'js13kgames\controllers'], function() use($router)
+		{
+			// Home
+			$router->get('/','Home@index');
+
+			// Entries
+			$router->group(['prefix' => 'entries'], function() use($router)
+			{
+				$router->get('', 'Entries@index');
+				$router->get('{slug}/{category}', 'Entries@index')->where('slug', ".+")->where('category', ".+");
+				$router->get('{slug}', 'Entries@show')->where('slug', ".+");
+			});
+		});
+	});
+
+	return $kernel;
