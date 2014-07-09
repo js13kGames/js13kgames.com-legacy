f95c711b59642fbdf5c362287686057f5f04d1f0
Michał Chojnacki <m.chojnacki@muyo.io>
2014-07-09 12:15:00 +0200

    [Migrations] Added a Competition tables migration.

---
+++ src/data/migrations/2014_07_03_154658_create_competition_tables.php
@@ -0,0 +1,83 @@
+<?php
+
+	// External dependencies
+	use Illuminate\Database\Schema\Blueprint;
+	use Illuminate\Database\Migrations\Migration;
+
+	/**
+	 * Migration: Create Competition Tables
+	 *
+	 * @package     Js13kgames\Data\Migrations
+	 * @version     0.1.0
+	 * @author      Michal Chojnacki <m.chojnacki@muyo.pl>
+	 * @copyright   2012-2014 js13kGames Team
+	 * @link        http://js13kgames.com
+	 */
+
+	class CreateCompetitionTables extends Migration
+	{
+		/**
+		 *
+		 */
+
+		public function up()
+		{
+			Schema::create('editions', function(Blueprint $table)
+			{
+				$table->increments('id');
+				$table->string('title', 50);
+				$table->string('slug', 50);
+			});
+
+			Schema::create('categories', function(Blueprint $table)
+			{
+				$table->increments('id');
+				$table->string('title', 50);
+				$table->string('slug', 50);
+				$table->integer('edition_id');
+			});
+
+			Schema::create('categories_submissions', function(Blueprint $table)
+			{
+				$table->integer('category_id');
+				$table->integer('submission_id');
+			});
+
+			Schema::create('submissions', function(Blueprint $table)
+			{
+				$table->increments('id');
+				$table->boolean('active')->default(0);
+				$table->string('title', 50);
+				$table->string('slug', 50);
+				$table->string('website_url', 100);
+				$table->string('server_url', 100);
+				$table->text('description')->nullable();
+				$table->integer('edition_id');
+				$table->timestamps();
+			});
+
+			Schema::create('repositories', function(Blueprint $table)
+			{
+				$table->increments('id');
+				$table->boolean('active')->default(0);
+				$table->integer('type');
+				$table->string('http_url', 255);
+				$table->integer('user_id');
+				$table->integer('submission_id');
+				$table->timestamps();
+			});
+		}
+
+		/**
+		 *
+		 */
+
+		public function down()
+		{
+			Schema::drop('editions');
+			Schema::drop('categories');
+			Schema::drop('submissions');
+			Schema::drop('repositories');
+			Schema::drop('categories_submissions');
+		}
+	}
