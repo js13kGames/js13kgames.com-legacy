151e9a8c3bb61ece053ff3644a0b51140f513516
Michał Chojnacki <m.chojnacki@muyo.io>
2014-07-09 14:15:11 +0200

    [Models] Added a Repository model.

---
+++ src/data/models/Repository.php
@@ -0,0 +1,38 @@
+<?php namespace js13kgames\data\models;
+
+	/**
+	 * Repository Model
+	 *
+	 * @package     Js13kgames\Data\Models
+	 * @version     0.0.1
+	 * @author      Michal Chojnacki <m.chojnacki@muyo.pl>
+	 * @copyright   2012-2014 js13kGames Team
+	 * @link        http://js13kgames.com
+	 */
+
+	class Repository extends Base
+	{
+		/**
+		 * @var array   The name of the table associated with the model.
+		 */
+
+		protected $table = 'repositories';
+
+		/**
+		 *
+		 */
+
+		public function user()
+		{
+			return $this->belongsTo('js13kgames\data\models\User');
+		}
+
+		/**
+		 *
+		 */
+
+		public function submission()
+		{
+			return $this->belongsTo('js13kgames\data\models\Submission');
+		}
+	}
