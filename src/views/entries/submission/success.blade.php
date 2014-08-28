e71dc36c8f96fb5e52ceb3307b0050b315e09809
Michał Chojnacki <m.chojnacki@muyo.io>
2014-08-28 08:24:10 +0200

    Grab the email from the proper object.

--- src/views/entries/submission/success.blade.php
+++ src/views/entries/submission/success.blade.php
@@ -2,6 +2,6 @@
 @section('content')
 <p class="message success">
 	<b>Success!</b> Your submission has been saved. However, it still needs to be verified before it becomes available.
-	You will receive an e-mail to the address <b>{{ $submission->email }}</b> when it gets accepted or rejected.
+	You will receive an e-mail to the address <b>{{ $submission->user->email }}</b> when it gets accepted or rejected.
 </p>
 @stop
