45500e6ff44c0b5d0c7b26f6c7700699447e99e9
satanas <wil.alejandro@gmail.com>
2016-06-17 18:53:01 -0400

    Created basic structure to render home pages according the year passed in the URL

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 'use strict';
 
 var express = require('express');
+var hbs = require('express-hbs');
 var sqlite3 = require('sqlite3').verbose();
 
 var app = express();
 var db = new sqlite3.Database('production.sqlite');
+
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views/partials',
+  layoutsDir: __dirname + '/views/layouts'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
   db.all('SELECT * FROM editions', function(err, rows) {
     rows.forEach(function(row) {
       console.log(row.id + " - " + row.title);
     });
-    res.send('ok');
+    res.render('home', {is2015: true});
   });
 });
 
