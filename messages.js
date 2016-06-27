d685ebbb95db71216396045c26f68708ea08c070
satanas <wil.alejandro@gmail.com>
2016-06-27 15:24:31 -0400

    Adding more validation messages

--- messages.js
+++ messages.js
@@ -1,6 +1,10 @@
 var messages = {
   error: {
-    slugNotUnique: 'A game with that name has already been submitted. Please choose a different name.'
+    slugNotUnique: 'A game with that name has already been submitted. Please choose a different name.',
+    invalidZipFormat: 'You have to submit a valid zip.',
+    invalidZipSize: 'Your game exceeds the 13.312 bytes limits.',
+    invalidImageFormat: 'Invalid format for image. Use jpeg, gif or png.',
+    invalidImageSize: 'Images can not exceed 100kb.'
   }
 }
 
